27ec972a5620acbf55f2f38b7d8bf4716a1318a8
DK\msb <msb@sitecore.net>
2017-04-27 16:41:50 -0400

    Revert "Custom values are saved in the Sent message task pool instead of enagement plan state."
    This reverts commit c9186c3cd13394dd2b631b19e3fb2cb6e89550c7.

--- src/Sitecore.Support.159397/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Sitecore.Support.159397")]
-[assembly: AssemblyProduct("Sitecore.Support.159397")]
-[assembly: ComVisible(false)]
